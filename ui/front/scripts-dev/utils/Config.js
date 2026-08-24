5937717deecb61a0626207997431840404f0f2da
LandDuck <300225311+LandDuck@users.noreply.github.com>
2026-08-25 02:15:39 +0800

    1.0.5

--- ui/front/scripts-dev/utils/Config.js
+++ ui/front/scripts-dev/utils/Config.js
@@ -36,7 +36,7 @@ class Config {
             apiUrl = "https://edutest.retail.tencent.com/webapi/";//测试地址
         }*/
         this.data = {
-            version: "1.0.4",
+            version: "1.0.5",
             apiUrl: apiUrlByCurrentUrl
         }
     }
